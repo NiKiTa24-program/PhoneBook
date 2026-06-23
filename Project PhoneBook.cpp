c997154d2ca7e8b5f25982779cd07035132993c1
Nikita <nk2411206@gmail.com>
2026-06-23 13:58:37 +0300

    Change a problem with file

--- Project PhoneBook.cpp
+++ Project PhoneBook.cpp
@@ -28,7 +28,7 @@ private:
     map<string, Contact> contacts;
 public:
     void addContact(const Contact& c) {
-        contacts[c.getName()] = c.getPhone();
+        contacts[c.getName()] = c;
     }
 
     void removeContact(const string& name) {
@@ -68,24 +68,29 @@ public:
             cout << "Не удалось открыть файл для записи" << endl;
         }
         for (const auto& pair : contacts) {
-            file << pair.first << ": " << pair.second.changePhone() << endl;
-            file.close();
+            file << pair.first << ": " << pair.second.getPhone() << endl;
             cout << "Контакты успешно сохрнались в файле " << filename << endl;
         }
+        file.close();
     }
 
     void loadFromFile(const string& filename) {
         ifstream file(filename);
         if (!file.is_open()) {
             cout << "Не удалось открыть файл для чтения" << endl;
+            return;
         }
-        else {
-            string line;
-            while (getline(file, line)) {
-                cout << line << endl;
+        string line;
+        while (getline(file, line)) {
+            size_t pos = line.find(": ");
+            if (pos != string::npos) {
+                string name = line.substr(0, pos);
+                string phone = line.substr(pos + 2);
+                contacts[name] = Contact(name, phone);
             }
-            file.close();
         }
+        file.close();
+        cout << "Контакты загружены из " << filename << endl;
     }
 };
 
@@ -96,12 +101,26 @@ class Menu {
 int main() {
     setlocale(LC_ALL, "RU");
 
+    PhoneBook book;
+
     Contact c1("Анна", "89171234567");
     Contact c2("Борис", "89271234567");
 
     c1.display();
     c2.display();
     
+    // Добавляем
+    book.addContact(Contact("Анна", "89171234567"));
+    book.addContact(Contact("Борис", "89271234567"));
+
+    // Сохраняем
+    book.saveToFile("contacts.txt");
+
+    // Загружаем
+    book.loadFromFile("contacts.txt");
+
+    // Выводим
+    book.displayAll();
 
     return 0;
 }
